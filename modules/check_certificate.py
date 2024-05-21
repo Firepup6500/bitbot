3bf47d9ded8daffc4e710a17a3289744c0bff67a
Firepup Sixfifty <firepyp650@gmail.com>
2024-05-20 22:23:16 -0500

    Bitbot Python3.11 Inital Commit

--- modules/check_certificate.py
+++ modules/check_certificate.py
@@ -15,8 +15,8 @@ class Module(ModuleManager.BaseModule):
             today = datetime.datetime.utcnow().date()
             week = datetime.timedelta(days=7)
 
-            not_valid_until = (today-certificate.not_valid_before.date()).days
-            not_valid_after = (certificate.not_valid_after.date()-today).days
+            not_valid_until = (today-certificate.not_valid_before_utc.date()).days
+            not_valid_after = (certificate.not_valid_after_utc.date()-today).days
 
             if not_valid_until < 0:
                 self.log.warn(
